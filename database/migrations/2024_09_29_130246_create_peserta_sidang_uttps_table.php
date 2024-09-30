744a9ecab1f56b078db034dc8d5df172b9158080
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2024-09-30 20:47:34 +0700

    penamabhan table

---
+++ database/migrations/2024_09_29_130246_create_peserta_sidang_uttps_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peserta_sidang_uttps', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("peserta_sidang_id");
+            $table->unsignedBigInteger('uttp_id');
+            $table->integer('jumlah')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peserta_sidang_uttps');
+    }
+};
